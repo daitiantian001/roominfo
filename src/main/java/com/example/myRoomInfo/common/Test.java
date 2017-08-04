16e46b5518dd7df5f703d9904d261a17a9697b2e
daitiantian001 <just2do@aliyun.com>
2017-08-04 17:30:33 +0800

    add test

---
+++ src/main/java/com/example/myRoomInfo/common/Test.java
@@ -0,0 +1,10 @@
+package com.example.myRoomInfo.common;
+
+/**
+ * Created by daitian on 2017/8/4.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+}
